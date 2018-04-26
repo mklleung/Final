de0e73ea45bbf9b1a58cf4920a071d129046cf76
nagassoway <33837594+nagassoway@users.noreply.github.com>
2018-04-26 00:01:46 -0400

    Edited rotates yet again

--- game0.js
+++ game0.js
@@ -129,7 +129,7 @@ The user moves a cube around the board trying to knock balls into a cone
 
 		var box1 = createBox2();
 		box1.position.set(10,3,-10);
-		box1.rotateX(90)
+		box1.rotateZ(90)
 		box1.addEventListener( 'collision',
 				function( other_object, relative_velocity, relative_rotation, contact_normal ) {
 					if (other_object==avatar){
@@ -141,7 +141,7 @@ The user moves a cube around the board trying to knock balls into a cone
 		
 		var box2 = createBox2();
 		box2.position.set(15,9,-10);
-		box2.rotateX(90)
+		box2.rotateZ(90)
 		box2.addEventListener( 'collision',
 			function( other_object, relative_velocity, relative_rotation, contact_normal ) {
 				if (other_object==avatar){
@@ -153,7 +153,7 @@ The user moves a cube around the board trying to knock balls into a cone
 		
 		var box3 = createBox2();
 		box3.position.set(30,3,-10);
-		box3.rotateX(90)
+		box3.rotateZ(90)
 		box3.addEventListener( 'collision',
 			function( other_object, relative_velocity, relative_rotation, contact_normal ) {
 				if (other_object==avatar){
